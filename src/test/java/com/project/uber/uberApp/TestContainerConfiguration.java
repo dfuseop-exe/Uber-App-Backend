4c8f76abd6c95fdeebe2151fd1e1a5730565f6e2
dfuseop-exe <sushant.165.shinde@gmail.com>
2025-02-08 17:16:31 +0530

    Testing

---
+++ src/test/java/com/project/uber/uberApp/TestContainerConfiguration.java
@@ -0,0 +1,20 @@
+package com.project.uber.uberApp;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+@TestConfiguration
+public class TestContainerConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        var image = DockerImageName.parse("postgis/postgis:12-3.0")
+                .asCompatibleSubstituteFor("postgres");
+        return new PostgreSQLContainer<>(image);
+    }
+}
--- src/test/java/com/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/project/uber/uberApp/UberAppApplicationTests.java
@@ -10,25 +10,4 @@ import java.util.List;
 @SpringBootTest
 class UberAppApplicationTests {
 
-	@Autowired
-	EmailService emailService;
-
-	@Test
-	void contextLoads() {
-		emailService.sendEmail(
-				"its.abhi917@gmail.com" ,
-				"Test Demo Email" ,
-				"Dummy Email From Uber"
-		);
-	}
-
-	@Test
-	void sendBulkEmail() {
-		String[] email = {"its.abhi917@gmail.com" , "sushant.165.shinde@gmail.com"};
-		emailService.sendEmail(
-                email,
-				"Test Demo Email" ,
-				"Dummy Email From Uber"
-		);
-	}
 }
